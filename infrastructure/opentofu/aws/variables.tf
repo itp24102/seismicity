57fabcf8b5f93fbe70fb7fc9047600edade67140
itp24102 <itp24102@hua.gr>
2025-06-22 02:43:48 +0300

    modified:   infrastructure/opentofu/aws/variables.tf

--- infrastructure/opentofu/aws/variables.tf
+++ infrastructure/opentofu/aws/variables.tf
@@ -5,3 +5,12 @@ variable "region" {
 variable "s3_bucket_name" {
   default = "seismicity-app-bucket"
 }
+variable "influx_url" {
+  description = "The URL of the InfluxDB instance"
+  type        = string
+}
+
+variable "influx_token" {
+  description = "The token for authenticating with InfluxDB"
+  type        = string
+}
