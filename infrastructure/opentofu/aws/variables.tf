1a8ecc4ac5c0ebef2822326b687b1f2630b890b5
itp24102 <itp24102@hua.gr>
2025-06-22 03:02:52 +0300

    modified:   infrastructure/opentofu/aws/variables.tf

--- infrastructure/opentofu/aws/variables.tf
+++ infrastructure/opentofu/aws/variables.tf
@@ -1,16 +1,23 @@
 variable "region" {
-  default = "eu-west-1"
+  description = "The AWS region to deploy resources to"
+  type        = string
+  default     = "eu-west-1"
 }
 
 variable "s3_bucket_name" {
-  default = "seismicity-app-bucket"
+  description = "The name of the S3 bucket for Lambda function code"
+  type        = string
+  default     = "seismicity-app-bucket"
 }
+
 variable "influx_url" {
   description = "The URL of the InfluxDB instance"
   type        = string
+  default     = "http://dummy"
 }
 
 variable "influx_token" {
   description = "The token for authenticating with InfluxDB"
   type        = string
+  default     = "dummy"
 }
