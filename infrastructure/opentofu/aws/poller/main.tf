6b2d55dc9735faf9d702c7da00adb11f8cf02934
itp24102 <itp24102@hua.gr>
2025-06-22 03:30:59 +0300

    modified:   infrastructure/opentofu/aws/poller/main.tf 	modified:   infrastructure/opentofu/aws/poller/variables.tf 	modified:   services/poller/Jenkinsfile

--- infrastructure/opentofu/aws/poller/main.tf
+++ infrastructure/opentofu/aws/poller/main.tf
@@ -2,48 +2,18 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_iam_role" "poller_lambda_role" {
-  name = "seismicity-poller-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Principal = { Service = "lambda.amazonaws.com" },
-      Effect = "Allow"
-    }]
-  })
-}
-
-resource "aws_iam_role_policy" "poller_lambda_policy" {
-  name = "poller-s3-policy"
-  role = aws_iam_role.poller_lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = [
-        "s3:*"
-      ],
-      Effect = "Allow",
-      Resource = "*"
-    }]
-  })
-}
-
-resource "aws_lambda_function" "poller" {
+resource "aws_lambda_function" "seismicity" {
   function_name = "seismicity-function"
-  role          = aws_iam_role.poller_lambda_role.arn
+  role          = var.lambda_role_arn
   handler       = "handler.handler"
   runtime       = "python3.9"
 
-  s3_bucket        = var.s3_bucket
-  s3_key           = "function.zip"
-  source_code_hash = filebase64sha256("function.zip")
+  s3_bucket = var.s3_bucket_name
+  s3_key    = "function.zip"
 
   environment {
     variables = {
-      S3_BUCKET     = var.s3_bucket
+      S3_BUCKET     = var.s3_bucket_name
       S3_KEY_PREFIX = "events/"
       AWS_REGION    = var.region
     }
--- infrastructure/opentofu/aws/poller/variables.tf
+++ infrastructure/opentofu/aws/poller/variables.tf
@@ -1,9 +1,14 @@
 variable "region" {
-  description = "AWS Region"
+  description = "The AWS region to deploy into"
   type        = string
 }
 
-variable "s3_bucket" {
-  description = "S3 bucket where Lambda code is uploaded"
+variable "s3_bucket_name" {
+  description = "Name of the S3 bucket containing the function.zip"
+  type        = string
+}
+
+variable "lambda_role_arn" {
+  description = "ARN of the IAM role for Lambda execution"
   type        = string
 }
