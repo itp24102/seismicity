f00d8a0a76f6f9e4fd74962d0775555ff1dd2ba6
itp24102 <itp24102@hua.gr>
2025-06-24 18:21:24 +0300

    modified:   infrastructure/opentofu/aws/poller/scheduler.tf

--- infrastructure/opentofu/aws/poller/scheduler.tf
+++ infrastructure/opentofu/aws/poller/scheduler.tf
@@ -2,7 +2,7 @@ resource "aws_cloudwatch_event_rule" "poller_schedule" {
   name                = "poller-schedule"
   description         = "Trigger seismicity-function every 5 minutes"
   schedule_expression = "rate(5 minutes)"
-  is_enabled          = true
+  state               = "ENABLED"
 }
 
 resource "aws_cloudwatch_event_target" "poller_lambda_target" {
